bc2b1c048ce26888a778d16900a17df896f980ea
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-09 14:25:11 +0100

    ♻️ Update : add new hook in modal to make it more interactive and customizable

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,14 @@
-import {forwardRef} from 'react';
+import {forwardRef,useImperativeHandle, useRef} from 'react';
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    const dialog = useRef();
+    useImperativeHandle(ref, () => ({
+        open() {
+            dialog.current.showModal();
+        }
+    }));
     return (
-        <dialog  ref={ref} className="result-modal">
+        <dialog  ref={dialog} className="result-modal">
             <div className="modal-content">
                 <h2>You {result}</h2>
                 <p>The target time was <strong>{targetTime} seconds.</strong></p>
