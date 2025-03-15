3861f7bd1858313fff870c8f648f92f33088cbfc
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-15 23:04:12 +0100

    ♻️ Update : Result Modal add create portal and score calculation

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,25 +1,31 @@
 import {forwardRef,useImperativeHandle, useRef} from 'react';
+import { createPortal } from 'react-dom';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onRest }, ref) {
     const dialog = useRef();
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime /  (targetTime*1000))*100);
     useImperativeHandle(ref, () => ({
         open() {
             dialog.current.showModal();
         }
     }));
-    return (
-        <dialog  ref={dialog} className="result-modal">
+    return createPortal(
+        <dialog  ref={dialog} className="result-modal" onClose={onRest}>
             <div className="modal-content">
-                <h2>You {result}</h2>
+               {userLost && <h2>You Lost</h2>}
+               {!userLost && <h2>Your Score: {score}</h2>}
                 <p>The target time was <strong>{targetTime} seconds.</strong></p>
                 <p>
-                    You stopped the timer at <strong>X seconds left</strong>.
+                    You stopped the timer at <strong>{formattedRemainingTime} seconds left</strong>.
                 </p>
             </div>
-            <form method="dialog">
+            <form method="dialog" onSubmit={onRest}>
             <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     );
 });
 export default ResultModal;
